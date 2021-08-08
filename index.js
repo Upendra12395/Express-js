d7827a0528d35be0ab91752b13f677488ce272e4
Upendra12395 <ch95upendra@gmail.com>
2021-08-09 00:02:52 +0530

    route for html file'

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const PORT = 4000;
+
 app.get('/', (req, res)=>{
     res.send("hello");
 })
+
+app.get('/s', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
 app.listen(PORT, ()=>{
     console.log(`app listening on ${PORT}`);
 })
