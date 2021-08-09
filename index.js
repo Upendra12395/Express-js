8161c686e41454662ba3846d718671cbb08951c6
Upendra12395 <ch95upendra@gmail.com>
2021-08-10 00:39:40 +0530

    static demo

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express()
 
 const PORT = 4000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res)=>{
     res.send("hello");
 })
 
-app.get('/s', (req, res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+//app.get('/s', (req, res)=>{
+//    res.sendFile(path.join(__dirname, 'public/index.html'))
+//})
 
 app.listen(PORT, ()=>{
     console.log(`app listening on ${PORT}`);
