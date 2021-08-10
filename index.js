10e1fc183d9efdf65a4e7648addeff517eeba637
Upendra12395 <ch95upendra@gmail.com>
2021-08-10 22:34:21 +0530

    new exercise middleware

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
-const path = require('path')
+const path = require('path');
+const users = require('./userslist/Users')
+const logger = require('./middleware/logger')
+const moment = require('moment')
 const app = express()
 
 const PORT = 4000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+//init middleware
+app.use(logger);
+
+app.get('/api/users', (req, res) =>{
+    res.json(users);
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res)=>{
     res.send("hello");
---
+++ userslist/Users.js
@@ -0,0 +1,19 @@
+const users =[{
+    id : 1,
+    name : "Ajay",
+    course : "Java"
+},{
+    id : 2,
+    name : "Ravi",
+    course : "C++"
+},{
+    id : 3,
+    name : "Rahul",
+    course : "HTML"
+},{
+    id : 4,
+    name : "Rohan",
+    course : "JavaScript"
+}];
+
+module.exports = users
